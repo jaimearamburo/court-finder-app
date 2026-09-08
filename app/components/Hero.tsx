8456386260a38926aad5b8126fb07184b143325b
JaimeA <jaimme.a@hotmail.com>
2026-09-08 15:40:00 +1000

    - Update hero section.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,11 +3,11 @@ export default function Hero() {
     <div className="relative bg-gradient-to-r from-orange-100 to-pink-100 sm:rounded-md overflow-hidden mb-2 md:mb-5 -mx-2 sm:mx-0">
 
       <div className="p-4 md:p-6 relative z-20">
-        <h1 className="text-2xl md:text-5xl font-bold text-black mb-0 md:mb-3">
-          Reilly, wanna play? <span className="">Play</span>.
+        <h1 className="text-2xl md:text-5xl font-bold text-black mb-0 md:mb-3 -ml-1">
+          Game on.
         </h1>
-        <p className="text-gray-600 text-sm md:text-lg">
-          Search <b>current</b> availability for your favorite sports.
+        <p className="text-gray-600 text-sm md:text-lg p">
+          One search. Every club. <b>Real time</b> availability - right now.
         </p>
       </div>
     </div>
